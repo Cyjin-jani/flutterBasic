a2f960487a74a6f6347616a00b17bd7b791a204a
yjcho <jinjanic91@gmail.com>
2022-06-26 21:33:55 +0900

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,45 @@
+import 'package:basic/widget/bottom_bar.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  late TabController controller;
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'JinFlix',
+      theme: ThemeData(
+          accentColor: Colors.white,
+          primaryColor: Colors.black,
+          brightness: Brightness.dark),
+      home: DefaultTabController(
+        length: 4,
+        child: Scaffold(
+          body: TabBarView(
+            physics: NeverScrollableScrollPhysics(),
+            children: <Widget>[
+              Container(
+                child: Center(child: Text('home')),
+              ),
+              Container(
+                child: Center(child: Text('search')),
+              ),
+              Container(
+                child: Center(child: Text('save')),
+              ),
+              Container(
+                child: Center(child: Text('more')),
+              ),
+            ],
+          ),
+          bottomNavigationBar: Bottom(),
+        ),
+      ),
+    );
+  }
+}
